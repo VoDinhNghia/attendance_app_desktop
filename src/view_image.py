9818381134aa48bc60541515e9007a552c63bd06
VoDinhNghia <vodinhnghia85@gmail.com>
2023-03-18 11:14:32 +0700

    Refactor code search by today

--- src/view_image.py
+++ src/view_image.py
@@ -2,15 +2,15 @@ from cv2 import*
 import cv2
 import os
 
-class Xem_Image:
-    def __init__(self, path, Id):
+class ViewImage:
+    def __init__(self, path, id):
         self.path = path
-        self.Id = Id
+        self.id = id
 
-    def Xem(self):
-        imagePaths=[os.path.join(self.path,f) for f in os.listdir(self.path)] 
+    def view(self):
+        imagePaths = [os.path.join(self.path, f) for f in os.listdir(self.path)] 
         for imagePath in imagePaths:
-            ID_xem_anh=int(os.path.split(imagePath)[-1].split('.')[1])
-            if(ID_xem_anh == self.Id):
-                img_xem_anh = cv2.imread(imagePath)
-                cv2.imshow("Xem anh",img_xem_anh)
+            idImage = int(os.path.split(imagePath)[-1].split('.')[1])
+            if(idImage == self.id):
+                showImage = cv2.imread(imagePath)
+                cv2.imshow('View image', showImage)
